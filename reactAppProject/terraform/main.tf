3df8b38d27c920c52531e7fb77575b3447bead94
Oluwasanya Ogunsakin <119258398+sanyo123@users.noreply.github.com>
2023-10-29 09:26:30 +0000

    Update main.tf

--- reactAppProject/terraform/main.tf
+++ reactAppProject/terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "sanyo123bucketss"
+    region         = "us-east-1"
+    key            = "backend.tfstate"
+    dynamodb_table = "sanyo123tables"
+  }
+}
 provider "aws" {
   region = "us-east-2"
 }
